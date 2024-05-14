5aa1e529d7bfffa65a356a2c604e99d2d9905497
utilisateur <utilisateur@G650-09.iutcb.univ-littoral.fr>
2024-05-14 15:56:25 +0200

    ajout tavalier

--- Tavalier.java
+++ Tavalier.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
 
-class tavalier extends Piece {
+class Tavalier extends Piece {
     
-    public tavalier(){
+    public Tavalier(){
         super('B', new Position());
     }
 
-    public tavalier(char couleur, Position position){
+    public Tavalier(char couleur, Position position){
         super(couleur, position);
     }
 
